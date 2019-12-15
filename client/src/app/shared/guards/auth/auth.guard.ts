0e7f917ca92e62aa970a879a57f5ae1f50b7ba00
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2019-12-15 22:26:03 +0300

    Task-9 Change logic of auth guard in order to return Observable as a value in canActivate.

--- client/src/app/shared/guards/auth/auth.guard.ts
+++ client/src/app/shared/guards/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { AuthorizationService } from '../../../core/authorization/authorization.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -15,9 +15,9 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (this.authService.isAuth()) {
-        return true;
+        return of(true);
       }
       this.router.navigate(['/login']);
-      return false;
+      return of(false);
   }
 }
