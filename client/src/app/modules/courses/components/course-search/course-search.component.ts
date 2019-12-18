6d9a2ec6756780c2f4ea551922e211c61ea00b0f
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2019-12-18 20:30:59 +0300

    Task-9 refactoring of the code

--- client/src/app/modules/courses/components/course-search/course-search.component.ts
+++ client/src/app/modules/courses/components/course-search/course-search.component.ts
@@ -1,29 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { Observable } from 'rxjs';
-import { filter, debounceTime } from 'rxjs/operators';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject} from 'rxjs';
+import { filter, debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-course-search',
   templateUrl: './course-search.component.html',
   styleUrls: ['./course-search.component.less']
 })
-export class CourseSearchComponent {
+export class CourseSearchComponent implements  OnInit, OnDestroy {
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
+  public courseSearch: Subject<string> = new Subject<string>();
+  private componentDestroyed = new Subject();
 
-  public handleSearch(searchValue: string): void {
-
-    const observable = new Observable(observer => {
-      observer.next(searchValue);
-    });
-
-    observable
+  ngOnInit(): void {
+    this.courseSearch
       .pipe(
         filter( (query: string) => !query || query.length >= 3),
-        debounceTime(200)
+        debounceTime(200),
+        takeUntil(this.componentDestroyed),
       )
       .subscribe((query: string) => {
-      this.search.emit(query);
+        this.search.emit(query);
     });
   }
 
+  ngOnDestroy(): void {
+    this.componentDestroyed.next();
+    this.componentDestroyed.unsubscribe();
+  }
+
+  public handleSearch(searchValue: string): void {
+    this.courseSearch.next(searchValue);
+  }
 }
